fab7f77e29c8e778728bdad6845614859b392248
Ian Christian Myers <ian@iancmyers.com>
2018-05-06 16:45:08 -0700

    ⚙️ Adding ServiceWorker implementation

--- js/resume.js
+++ js/resume.js
@@ -1,5 +1,4 @@
 // ⚡️ Just a little JavaScript to detect online/offline status.
-
 window.addEventListener('online', function () {
   document.body.classList.remove('offline');
 });
@@ -7,3 +6,17 @@ window.addEventListener('online', function () {
 window.addEventListener('offline', function () {
   document.body.classList.add('offline');
 });
+
+if (!navigator.onLine) {
+  document.body.classList.add('offline');
+}
+
+// It's always nice to be friendly in the console.
+if ('console' in window) {
+  console.log('👋 You can find the full source here: https://github.com/iancmyers/resume.iancmyers.com');
+}
+
+// ⚙️ Let's over-engineer this a little more with a ServiceWorker.
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../service-worker.js');
+}
---
+++ service-worker.js
@@ -0,0 +1,44 @@
+self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+
+workbox.setConfig({ debug: false });
+workbox.googleAnalytics.initialize();
+
+workbox.core.setCacheNameDetails({
+  prefix: 'resume',
+  suffix: 'v2'
+});
+
+workbox.routing.registerRoute(
+  /\/$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'root-html',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 1 Week
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis'
+  }),
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images',
+  }),
+);
+
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'static-resources',
+  }),
+);
+
+
