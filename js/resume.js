eb776497a33211edc2eabde67269a6dcd471a5fd
Ian Christian Myers <ian@iancmyers.com>
2020-03-28 17:13:19 -0700

    Update role

--- js/resume.js
+++ js/resume.js
@@ -1,9 +1,9 @@
 // ⚡️ Just a little JavaScript to detect online/offline status.
-window.addEventListener('online', function() {
+window.addEventListener('online', function () {
   document.body.classList.remove('offline');
 });
 
-window.addEventListener('offline', function() {
+window.addEventListener('offline', function () {
   document.body.classList.add('offline');
 });
 
--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ workbox.googleAnalytics.initialize();
 
 workbox.core.setCacheNameDetails({
   prefix: 'resume',
-  suffix: 'v2'
+  suffix: 'v2',
 });
 
 workbox.routing.registerRoute(
@@ -14,29 +14,29 @@ workbox.routing.registerRoute(
     cacheName: 'root-html',
     plugins: [
       new workbox.expiration.Plugin({
-        maxAgeSeconds: 7 * 24 * 60 * 60 // 1 Week
-      })
-    ]
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 1 Week
+      }),
+    ],
   })
 );
 
 workbox.routing.registerRoute(
   new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
   workbox.strategies.cacheFirst({
-    cacheName: 'googleapis'
+    cacheName: 'googleapis',
   })
 );
 
 workbox.routing.registerRoute(
   /\.(?:png|gif|jpg|jpeg|svg)$/,
   workbox.strategies.cacheFirst({
-    cacheName: 'images'
+    cacheName: 'images',
   })
 );
 
 workbox.routing.registerRoute(
   /\.(?:js|css)$/,
   workbox.strategies.cacheFirst({
-    cacheName: 'static-resources'
+    cacheName: 'static-resources',
   })
 );
